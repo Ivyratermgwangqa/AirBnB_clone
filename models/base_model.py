0af914e26fa52241391395b711e1c4cf82fc1daa
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-12-07 04:34:57 +0200

    Update base_model.py
    The datetime attributes are handled according to the specified format, and the class is designed to be easily extendable for other classes in the project.

--- models/base_model.py
+++ models/base_model.py
@@ -16,17 +16,18 @@ class BaseModel:
             **kwargs (dict): Additional key/value pairs of attributes.
         """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+
+        if kwargs:
+            # If kwargs is not empty, create attributes based on i-j pairs
             for i, j in kwargs.items():
                 if i == "created_at" or i == "updated_at":
-                    self.__dict__[i] = datetime.strptime(j, tform)
-                else:
-                    self.__dict__[i] = j
-        else:
-            models.storage.new(self)
+                    setattr(self, i, datetime.strptime(j, tform))
+                elif i != "__class__":
+                    setattr(self, i, j)
+        else:/
+            # If kwargs is empty, create a new instance with id and created_at
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
 
     def save(self):
         """Update updated_at with the current datetime."""
@@ -49,3 +50,9 @@ class BaseModel:
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    @classmethod
+    def from_dict(cls, adict):
+        """Create a BaseModel instance from a dictionary representation."""
+        instance = cls(**adict)
+        return instance
