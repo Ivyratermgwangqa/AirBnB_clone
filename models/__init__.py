774a2b80c8433336437e2f7fdde0b0e15ad3e6f6
Dan2759 <kwizeradan.kd27@gmail.com>
2023-12-10 13:50:43 -0800

    up_Date

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 """Initialization module for the models package."""
 from models.engine.file_storage import FileStorage
 
-
+"""A var storage, an interger for fileStorage."""
 storage = FileStorage()
 storage.reload()
